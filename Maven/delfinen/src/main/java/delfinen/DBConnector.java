e3c2ae6152df8102c575829b90a3f4f8b5c54549
JoakimKss <Joakim.kss@gmail.com>
2019-12-02 15:42:58 +0100

    Comments

--- Maven/delfinen/src/main/java/delfinen/DBConnector.java
+++ Maven/delfinen/src/main/java/delfinen/DBConnector.java
@@ -1,4 +1,6 @@
 package delfinen;
 
 public class DBConnector {
+
+    //Class for connecting to DB and performing queries.
 }
--- Maven/delfinen/src/main/java/delfinen/DBQuery.java
+++ Maven/delfinen/src/main/java/delfinen/DBQuery.java
@@ -1,4 +1,6 @@
 package delfinen;
 
 public class DBQuery {
+
+    //Class with methods, defining specific queries to be used with the DBConnector class.
 }
