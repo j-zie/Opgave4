a255ab8113d12f175b08a0e1b5e48284d44047eb
Frederik Johnsen <frederik@familienjohnsen.dkgit config --global user.email frederik@familienjohnsen.dk>
2019-12-03 12:05:55 +0100

    Skelet til Menu class

--- Maven/delfinen/src/main/java/delfinen/DBQuery.java
+++ Maven/delfinen/src/main/java/delfinen/DBQuery.java
@@ -2,5 +2,12 @@ package delfinen;
 
 public class DBQuery {
 
+    //TODO place member data into an SQL query
+    public String insertMember(Member m) {
+        String result = "";
+
+        return result;
+    }
+
     //Class with methods, defining specific queries to be used with the DBConnector class.
 }
--- Maven/delfinen/src/main/java/delfinen/Menu.java
+++ Maven/delfinen/src/main/java/delfinen/Menu.java
@@ -6,4 +6,34 @@ public class Menu {
        Denne classe skal indeholde en switchcase som styres afhængigt af hvilken rolle, 'USER' har.
 
     */
+
+    Menu(User u) {
+        int ID = u.getRoleID();
+        switch (ID) {
+            case 1:
+                presidentMenu();
+                break;
+            case 2:
+                accountantMenu();
+                break;
+            case 3:
+                trainerMenu();
+                break;
+        }
+    }
+
+    //TODO code menu
+    private void presidentMenu() {
+
+    }
+
+    //TODO code menu
+    private void accountantMenu() {
+
+    }
+
+    //TODO code menu
+    private void trainerMenu() {
+
+    }
 }
